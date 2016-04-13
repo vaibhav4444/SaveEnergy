4d3b030f903b82a81ccac59bdc595396eeb7b823
Vaibhav singhal <vaibhav4444@gmail.com>
2016-04-13 23:07:39 +0530

    Initial commit

---
+++ app/src/main/java/com/poc/saveenergy/myapplication/receiver/WiFiReceiver.java
@@ -0,0 +1,27 @@
+package com.poc.saveenergy.myapplication.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
+/**
+ * Created by Vaib on 13-04-2016.
+ */
+public class WiFiReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        if(info != null && info.isConnected()) {
+            // Do your work.
+
+            // e.g. To check the Network Name or other info:
+            WifiManager wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+            WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+            String ssid = wifiInfo.getSSID();
+
+        }
+    }
+}
