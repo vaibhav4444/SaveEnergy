1a7cbfa5520d2b3a8d14c3f5e2110be869b5e155
Vaibhav singhal <vaibhav4444@gmail.com>
2016-04-13 23:35:49 +0530

    changes

--- app/src/main/java/com/poc/saveenergy/myapplication/MainActivity.java
+++ app/src/main/java/com/poc/saveenergy/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +34,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void onClick(View v){
+
     }
 }
